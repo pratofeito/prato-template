f3028875dfd4cf8b6762eae8daaa586330041885
mateuskrause <mateus.ke@outlook.com>
2023-07-09 11:48:01 -0300

    Basic template project working

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     // First, create a new game. Before starting, set initial state
     pte::Game game = pte::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Demo");
-    game.init_state<GameState>();
+    game.init_state<SplashState>();
     game.run();
 
     return 0;
